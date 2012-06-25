ccdc908ab9a0dea56c289616a03e06b5b761ded0
christianjoy <christianjoyventura@yahoo.com>
2012-06-25 10:07:51 +0800

    Program2

--- OOP_Menu.py
+++ OOP_Menu.py
@@ -1,4 +1,26 @@
-class Menu Item:
-    def _ _init_ _(self)
-            self.name=""
-            self.price=0
+class MenuItem:
+    def __init__(self):
+        self.name=""
+        self.price=0
+    def Show (self):
+        print("%s       %i" %(self.name,self.price))
+              
+Spag= MenuItem()
+Spag.name="Spaghetti"
+Spag.price=95
+
+print(Spag.name)
+print(Spag.price)
+
+
+Coke=MenuItem()
+Coke.name="Coke Float"
+Coke.price=25
+
+print(Spag.name)
+print(Spag.price)
+print(Coke.name)
+print(Coke.price)
+
+Spag.Show()
+Coke.Show()
