c22208ec3cf935b7ecaf4d14e1c42782fd6a4141
christianjoyventura <christianjoyventura@yahoo.com>
2012-08-04 08:46:47 +0800

    Instanciating and global variable

---
+++ Instanciating and global variable.py
@@ -0,0 +1,37 @@
+
+class Score
+    def __init__(self):
+        self.score=0
+    def Add (self):
+        self.score += 1
+    def show(self):
+        print("The score is %i" % (score))
+        
+Basketball=Score()
+Volleyball=Score()
+Chess=Score()
+
+
+
+Basketball.Add()
+Basketball.Add()
+Basketball.Add()
+Basketball.Add()
+Basketball.Add()
+Basketball.Add()
+Basketball.Add()
+Basketball.Add()
+Basketball.Add()
+Basketball.Add()
+Volleyball.Add()
+Volleyball.Add()
+Volleyball.Add()
+Volleyball.Add()
+Volleyball.Add()
+Chess.Add()
+Chess.Add()
+Chess.Add()
+
+Basketball.show()
+Volleyball.show()
+Chess.show()
