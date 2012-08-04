1d499f65d0c62061514215efc3083e4edf07d469
christianjoyventura <christianjoyventura@yahoo.com>
2012-08-04 08:48:22 +0800

    Class Song

---
+++ class song.py
@@ -0,0 +1,16 @@
+class Song:
+    def _init_(self):
+        self.title=None
+        self.genre=None
+        self.artist=None
+        
+class Pop(Song):
+    pass
+
+class Ballad(Song):
+    pass
+
+class SongsOfRicoBlanco(Song):
+    def ListAllSongs():
+        pass
+
